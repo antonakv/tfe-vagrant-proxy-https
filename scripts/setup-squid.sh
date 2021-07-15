0b258e4f1aa7d4675ce9e97303b747e09bae50cd
Anton Akulov <antonakv@users.noreply.github.com>
2021-07-15 16:22:53 +0200

    Adding cert to proxy config

--- scripts/setup-squid.sh
+++ scripts/setup-squid.sh
@@ -10,7 +10,11 @@ sed -i "s,#cache_dir ufs /var/spool/squid 100 16 256,cache_dir ufs /var/spool/sq
 
 sed -i "/INSERT YOUR OWN RULE/a http_access allow all" /etc/squid/squid.conf
 
-sed -i "s/http_port 3128/http_port 0.0.0.0:3128/" /etc/squid/squid.conf
+cp /vagrant/cert/192.168.56.32.nip.io.crt /etc/squid
+cp /vagrant/cert/192.168.56.32.nip.io.key /etc/squid
+chmod 600 /etc/squid/192.168.56.32.nip.io.key
+
+sed -i "s/http_port 3128/http_port 0.0.0.0:3128 cert=\/etc\/squid\/192.168.56.32.nip.io.crt key=\/etc\/squid\/192.168.56.32.nip.io.key/" /etc/squid/squid.conf
 
 systemctl restart squid
 
